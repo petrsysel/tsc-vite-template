023cc2319f609a25c052ab86b8dfcd3fca67de40
Petr Sysel <petrsysel99@gmail.com>
2023-12-26 01:51:31 +0100

    Šablona nastavena

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+function main(){
+    // Do stuff
+}
+
+window.addEventListener('load', () => {
+    main()
+})
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+describe("test", () => {
+    test("should pass", () => {
+        expect(1 + 2).toBe(3)
+    })
+    test("should fail", () => {
+        expect(3 - 2).toBe(0)
+    })
+})
